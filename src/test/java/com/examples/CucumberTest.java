434933a97fda487416d8355c3aaf745c1b8907cf
claxmika <claxmika@in.ibm.com>
2017-12-08 14:37:00 +0530

    cucumber.json report update

--- src/test/java/com/examples/CucumberTest.java
+++ src/test/java/com/examples/CucumberTest.java
@@ -5,7 +5,9 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/main/resources"},plugin = {"pretty"})
+@CucumberOptions(features = {"src/main/resources"},plugin = { "html:target/cucumber-html-report",
+        "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
+        "usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml" })
 
 public class CucumberTest {
 
